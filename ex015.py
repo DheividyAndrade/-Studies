d10509f30be12d11398bd37d3fc988fc4fbbc20f
Dheividy Andrade <deividyandrade@hotmail.com>
2024-06-09 13:25:21 -0300

    Update ex015.py

--- ex015.py
+++ ex015.py
@@ -1,9 +1,9 @@
 nome = str(input('Digite um nome: '))
 if nome == ('Dheividy'):
     print('Que nome Bonito!')
-elif nome == "Pedro" or nome == "Jose" or nome == "Andre":
+elif nome == "Ricardo" or nome == "Daniel" or nome == "Andre":
     print('Belo nome')
-elif nome == 'João':
+elif nome == 'João' or nome == 'Matheus' or nome == 'Pedro' or nome == 'Davi':
     print('Seu nome é BLIBLICO! ')
 else:
     print('Seu nome é bem comum ')
