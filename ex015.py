e2f1b3a88a1c6237fae2eb6b42cd26afd2258275
Dheividy Andrade <deividyandrade@hotmail.com>
2024-06-09 13:15:38 -0300

    Adicionei um exercicio

---
+++ ex015.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite um nome: '))
+if nome == ('Dheividy'):
+    print('Que nome Bonito!')
+elif nome == "Pedro" or nome == "Jose" or nome == "Andre":
+    print('Belo nome')
+else:
+    print('Seu nome é bem comum ')
+print('Tenha um bom dia, {}!'.format(nome))
