cbc5bb6eac9f7945ec94491778663a221378d713
Dheividy Andrade <deividyandrade@hotmail.com>
2024-06-14 19:44:22 -0300

    ex027, 28, 29, 30

---
+++ ex028.py
@@ -0,0 +1,5 @@
+# Faça a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+num = int(input('Digite um numero para saber sua tabuada:'))
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(num, c, num*c))
