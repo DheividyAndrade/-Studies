25cb794132fb73caa19eda3d05c7cb17813dc1f2
Dheividy Andrade <deividyandrade@hotmail.com>
2024-07-22 19:11:14 -0300

    ex064

---
+++ ex064.py
@@ -0,0 +1,11 @@
+# Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.   
+
+def escreva (msg):
+    print('-' * 20)
+    print(msg)
+    print('-' * 20)
+
+
+escreva('Dheividy Dhanrley')
+escreva('Masculino')
+escreva('28 anos')
